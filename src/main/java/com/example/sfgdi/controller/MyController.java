7eeba0ee35b70f1c7e315add6371425f8d7c9113
Madhukar <madhukar.reddy326@gmail.com>
2022-01-25 23:04:58 +0530

    Primary Annotation

--- src/main/java/com/example/sfgdi/controller/MyController.java
+++ src/main/java/com/example/sfgdi/controller/MyController.java
@@ -1,12 +1,21 @@
 package com.example.sfgdi.controller;
 
+import com.example.sfgdi.services.PrimaryBean;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    PrimaryBean  primaryBean;
+
+    public MyController(PrimaryBean primaryBean) {
+        this.primaryBean = primaryBean;
+    }
+
     public String sayHello()
     {
         System.out.println("Hello world!..");
-        return " Hi Folks";
+        //return " Hi Folks";
+        return primaryBean.sayGreeting();
+
     }
 }
