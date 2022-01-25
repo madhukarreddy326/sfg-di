a6706eb7454c57a1f7278148ba82f9c618bd2d90
Madhukar <madhukar.reddy326@gmail.com>
2022-01-25 22:23:21 +0530

    Dependency Injection without Spring

---
+++ src/main/java/com/example/sfgdi/controller/PropertyInjectedController.java
@@ -0,0 +1,14 @@
+package com.example.sfgdi.controller;
+
+import com.example.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    public GreetingService greetingService;
+
+    public String getGreeting()
+    {
+        return greetingService.sayGreeting();
+    }
+
+}
