d846561737b1991944d638925526aa013ea99028
Madhukar <madhukar.reddy326@gmail.com>
2022-01-25 22:42:26 +0530

    Dependency Injection With Spring

--- src/main/java/com/example/sfgdi/controller/SetterInjectedController.java
+++ src/main/java/com/example/sfgdi/controller/SetterInjectedController.java
@@ -1,9 +1,14 @@
 package com.example.sfgdi.controller;
 
 import com.example.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
+    @Autowired
     private GreetingService greetingService;
+
 
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
